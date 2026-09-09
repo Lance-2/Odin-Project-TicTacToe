c25a6cff74cedb8a9872adcb394fd295c81453c3
Lance-2 <lancekristanmartin@gmail.com>
2026-09-09 11:25:44 +0800

    Day 3 Progress
    Reset of scripts and HTML
    
    TO DO:
    - complete javascript backend logic

--- script.js
+++ script.js
@@ -1,122 +1,38 @@
-/*
- ** The Gameboard represents the state of the board
- ** Each square holds an X, O, or nothing (0) and is represented by a Cell 
- ** and we will use a 2d array to represent the board as a whole
- */
 
-function Gameboard() {
-  const rows = 3;
-  const columns = 3;
-  const board = [];
+const gameBoard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  const render = () => {
+    let boardHTML = "";
+    board.forEach((square, index) => {
+      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+    });
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+  };
+  return { render };  
+})();
 
-  // Create a 2d array that will represent the state of the game board
-  // For this 2d array, row 0 will represent the top row and
-  // column 0 will represent the left-most column.
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(Cell());
-    }
+const createPlayer = (name, symbol) => {
+  return { name, symbol };
+}
+
+const game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver = false;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector("#player1-name").value, "X"),
+      createPlayer(document.querySelector("#player2-name").value, "O")
+    ];
+    currentPlayerIndex = 0;
+    gameOver = false;
+    gameBoard.render();
   }
+  return { start };
+})();
 
-  // This will be the method of getting the entire board that our
-  // UI will eventually need to render it.
-  const getBoard = () => board;
-
-  // drop an X or O into a cell on the board. If the cell is already occupied, return false.
-  const putDownToken = (column, row, player) => {
-    if (board[row][column].getValue() === 0) {
-      board[row][column].addToken(player);
-      return true;
-    }
-    return false;
-  };
-
-  // This method will be used to print our board to the console.
-  // It is helpful to see what the board looks like after each turn as we play,
-  // but we won't need it after we build our UI
-  const printBoard = () => {
-    const boardWithCellValues = board.map((row) =>
-      row.map((cell) => cell.getValue())
-    );
-    console.log(boardWithCellValues);
-  };
-
-  // Here, we provide an interface for the rest of our
-  // application to interact with the board
-  return { getBoard, putDownToken, printBoard };
-}
-
-function Cell() {
-  let value = 0;
-  const addToken = (player) => {
-    value = player;
-  };
-  
-  const getValue = () => value;
-  return {
-    addToken,
-    getValue,
-  };
-}
-
-/*
- ** The GameController will be responsible for controlling the
- ** flow and state of the game's turns, as well as whether
- ** anybody has won the game
- */
-function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
-) {
-  const board = Gameboard();
-
-  const players = [
-    {
-      name: playerOneName,
-      token: 1,
-    },
-    {
-      name: playerTwoName,
-      token: 2,
-    },
-  ];
-
-  let activePlayer = players[0];
-
-  const switchPlayerTurn = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  };
-  const getActivePlayer = () => activePlayer;
-
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn.`);
-  };
-
-  const playRound = (column, row) => {
-    const success = board.putDownToken(row, column, getActivePlayer().token);
-    if(!success) {
-      console.log("That cell is already occupied. Please choose another cell.");
-      return;
-    }
-    /*  This is where we would check for a winner and handle that logic,
-          such as a win message. */
-
-    // Switch player turn
-    switchPlayerTurn();
-    printNewRound();
-  };
-
-  // Initial play game message
-  printNewRound();
-
-  // For the console version, we will only use playRound, but we will need
-  // getActivePlayer for the UI version, so I'm revealing it now
-  return {
-    playRound,
-    getActivePlayer,
-  };
-}
-
-const game = GameController();
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", () => {
+  game.start()
+});
