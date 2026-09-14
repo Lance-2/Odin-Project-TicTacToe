e5ef703c2628b3a3ed738b2a095bd3ca431c0a68
Lance-2 <lancekristanmartin@gmail.com>
2026-09-14 11:24:58 +0800

    Day 4 Game Logic Added
    To Do:
    Complete Missing Win, Tie Conditions
    Complete Reset Game Logic

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-
 const gameBoard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+  const placeMark = (index, symbol) => {
+    if (board[index] !== "") {
+      return false; // Square already occupied
+    } else {
+      board[index] = symbol;
+
+      return true;
+    }
+  };
+
   const render = () => {
     let boardHTML = "";
     board.forEach((square, index) => {
       boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
     });
     document.querySelector("#gameboard").innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square, index) => {
+      square.addEventListener("click", game.handleClick);
+    });
   };
-  return { render };  
+
+  return { render, getBoard, placeMark };
 })();
 
 const createPlayer = (name, symbol) => {
   return { name, symbol };
-}
+};
 
 const game = (() => {
   let players = [];
@@ -23,16 +39,27 @@ const game = (() => {
   const start = () => {
     players = [
       createPlayer(document.querySelector("#player1-name").value, "X"),
-      createPlayer(document.querySelector("#player2-name").value, "O")
+      createPlayer(document.querySelector("#player2-name").value, "O"),
     ];
     currentPlayerIndex = 0;
     gameOver = false;
     gameBoard.render();
-  }
-  return { start };
+  };
+  const switchPlayerTurn = () => {
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  };
+
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1]);
+    if (gameBoard.placeMark(index, players[currentPlayerIndex].symbol)) {
+      switchPlayerTurn();
+      gameBoard.render();
+    }
+  };
+  return { start, handleClick };
 })();
 
 const startButton = document.getElementById("start-button");
 startButton.addEventListener("click", () => {
-  game.start()
+  game.start();
 });
